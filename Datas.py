25e15ea052b59d8c7c628aa7e05b3c189e43d79f
GrayThing <samorodov-artem@mail.ru>
2024-08-01 22:06:04 +0300

    Выполнено практическое задание "Базовые структуры данных"

---
+++ Datas.py
@@ -0,0 +1,19 @@
+# Task 1
+# 1st program
+print(9 ** 0.5 * 5)
+
+# Task 2
+# 2nd program
+print(9.99 > 9.98 != 1000.1)
+
+# Task 3
+# 3rd program
+int_one = 1234
+int_two = 5678
+print(int(str(int_one)[1:3]) + int(str(int_two)[1:3]))
+
+# Task 4
+# 4th program
+float_one = 13.42
+float_two = 42.13
+print(int(str(float_one)[0:2]) == int(str(float_two)[3:5]) or int(str(float_two)[0:2]) == int(str(float_one)[3:5]))
