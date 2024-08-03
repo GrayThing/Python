3d179feadd0226286f5403e447aec9de43d9c77e
GrayThing <samorodov-artem@mail.ru>
2024-08-03 10:23:31 +0300

    Выполнено практическое задание 2023/09/22 00:00|Практическое задание по теме "Переменные".

---
+++ Variables.py
@@ -0,0 +1,5 @@
+hw_quantity_completed = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / hw_quantity_completed
+print(f"Курс:{course_name}, всего задач:{hw_quantity_completed}, затрачено часов:{hours_spent}, среднее время выполнения {time_per_task} часа")
